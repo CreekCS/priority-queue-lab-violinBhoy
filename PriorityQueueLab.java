e8e852af50d42b2153187756aed9f3fbc2af17bf
Andrew Sautel <152945937+violinBhoy@users.noreply.github.com>
2024-11-05 17:29:34 +0000

    created the code

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,26 @@
 // import statements
+import java.util.Scanner;
+import java.io.File;
+import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.io.FileNotFoundException;
 
 public class PriorityQueueLab {
-  
+    public static void main(String args[]) {
+        try {
+            File file = new File("RandIntegers.txt");
+            Scanner fileScanner = new Scanner(file);
+            PriorityQueue<Integer> ints = new PriorityQueue<Integer>();
+            while (fileScanner.hasNextLine()){
+                ints.add(Integer.parseInt(fileScanner.nextLine()));
+            }
+            int length = ints.size();
+            for (int i = 0; i < length; i++) {
+                System.out.println(ints.remove());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
 }
 
